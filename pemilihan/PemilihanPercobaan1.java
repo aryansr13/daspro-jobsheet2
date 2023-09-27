75c826bcb62c7910a115f6f5fb03d8ada6e83997
aryansr13 <rukunanggudi195@gmail.com>
2023-09-27 08:57:53 +0700

    jobsheet 5

---
+++ pemilihan/PemilihanPercobaan1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan1 {
+    public static void main(String[] args) {
+        Scanner input05 = new Scanner(System.in);
+        System.out.println("Masukkan angka");
+        int angka = input05.nextInt();
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " bilangan genap ");
+        } else {
+        }
+        System.out.println("Angka " + angka + " bilangan ganjil ");
+    }
+}
