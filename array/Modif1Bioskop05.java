a3da61b09103e0e077bb9574901215a42923a36b
aryansr13 <rukunanggudi195@gmail.com>
2023-11-02 13:07:58 +0700

    Jobsheet 10

---
+++ array/Modif1Bioskop05.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Modif1Bioskop05 {
+    public static void main(String[] args) {
+
+        String[][] penonton = new String[4][2];
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
+        System.out.println("Jumlah baris: " + penonton.length);
+        System.out.println("Panjang baris: " + penonton[0].length);
+
+        // Mencetak panjang setiap baris
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+
+        // Mencetak data pada baris ke-3
+        System.out.println("Penonton pada baris ke-3:");
+        for (String penontonBaris3 : penonton[2]) {
+            System.out.println(penontonBaris3);
+        }
+
+        // Mencetak semua data dalam array
+        System.out.println("Data seluruh array:");
+        for (String[] baris : penonton) {
+            for (String data : baris) {
+                System.out.print(data + " ");
+            }
+            System.out.println(); // Baris baru setelah mencetak semua data dalam satu baris
+        }
+    }
+}
